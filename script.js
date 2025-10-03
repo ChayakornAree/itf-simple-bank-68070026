96042e061115544ac26aaacc05dc6424416578fc
ChayakornAree <68070026@kmitl.ac.th>
2025-10-03 10:22:15 +0700

    update no css
    no css and text add not from abrove

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 const textarea = document.querySelector(".show-data")
-const account_balance_account = document.querySelector(".current-account-input").value
-const cash_balance_account = document.querySelector(".current-cash-input").value
 
+let account_balance_account = document.querySelector(".current-account-input").value
+let cash_balance_account = document.querySelector(".current-cash-input").value
 let count = 1
 let int_account_balance = Number(account_balance_account)
 let int_cash_balance = Number(cash_balance_account)
 
-let new_data = `${count}, Current balance: ${}, Current cash balance: ${cash_balance_account}`
+let new_data = `${count}, Current balance: ${account_balance_account}, Current cash balance: ${cash_balance_account}`
+textarea.value += (textarea.value ? "\n" : "") + new_data
+count ++;
 
-textarea.value += (textarea.value ? "\n" : "") + 
+function handleChange(){
+    account_balance_account = document.querySelector(".current-account-input").value
+    cash_balance_account = document.querySelector(".current-cash-input").value
+    new_data = `${count}, Current balance: ${account_balance_account}, Current cash balance: ${cash_balance_account}`
+    textarea.value += (textarea.value ? "\n" : "") + new_data
+    int_account_balance = Number(account_balance_account)
+    int_cash_balance = Number(cash_balance_account)
+    count ++;
+}
+
+function handleOperate(){
+    let command = document.querySelector(".command-select").value
+    let amount = document.querySelector(".amount-command").value
+    let amount_convert = Number(amount)
+    if (command == "deposit"){
+        if (amount_convert <= int_cash_balance){
+            int_account_balance += amount_convert
+            int_cash_balance -= amount_convert
+            new_data = `${count}, Current balance: ${(int_account_balance).toString()}, Current cash balance: ${(int_cash_balance).toString()}`
+            textarea.value += (textarea.value ? "\n" : "") + new_data
+            count ++
+        }else{
+            new_data = `${count}, Couldn't deposit entered balance. (Insufficient cash balance)`
+            textarea.value += (textarea.value ? "\n" : "") + new_data
+            count ++
+        }
+    }else{
+        if (amount_convert <= int_account_balance){
+            int_cash_balance += amount_convert
+            int_account_balance -= amount_convert
+            new_data = `${count}, Current balance: ${(int_account_balance).toString()}, Current cash balance: ${(int_cash_balance).toString()}`
+            textarea.value += (textarea.value ? "\n" : "") + new_data
+            count ++
+        }else{
+            new_data = `${count}, Couldn't withdraw entered balance. (Insufficient account balance)`
+            textarea.value += (textarea.value ? "\n" : "") + new_data
+            count ++
+        }
+    }
+}
 
