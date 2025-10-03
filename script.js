2224f47f9b6ecfe8c0177ad21aae479216c707b3
ChayakornAree <68070026@kmitl.ac.th>
2025-10-03 10:37:47 +0700

    UPDATE FUNCTION JS ADD DATA ABROBE TEXTAREA

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ let cash_balance_account = document.querySelector(".current-cash-input").value
 let count = 1
 let int_account_balance = Number(account_balance_account)
 let int_cash_balance = Number(cash_balance_account)
-
+let arr = []
 let new_data = `${count}, Current balance: ${account_balance_account}, Current cash balance: ${cash_balance_account}`
 textarea.value += (textarea.value ? "\n" : "") + new_data
+arr.unshift(new_data)
 count ++;
+
 
 function handleChange(){
     account_balance_account = document.querySelector(".current-account-input").value
     cash_balance_account = document.querySelector(".current-cash-input").value
     new_data = `${count}, Current balance: ${account_balance_account}, Current cash balance: ${cash_balance_account}`
-    textarea.value += (textarea.value ? "\n" : "") + new_data
+    textarea.value = ""
+    arr.unshift(new_data)
+    arr.forEach((data) =>{
+        textarea.value += data + "\n"
+    })
     int_account_balance = Number(account_balance_account)
     int_cash_balance = Number(cash_balance_account)
     count ++;
@@ -29,11 +35,19 @@ function handleOperate(){
             int_account_balance += amount_convert
             int_cash_balance -= amount_convert
             new_data = `${count}, Current balance: ${(int_account_balance).toString()}, Current cash balance: ${(int_cash_balance).toString()}`
-            textarea.value += (textarea.value ? "\n" : "") + new_data
+            textarea.value = ""
+            arr.unshift(new_data)
+            arr.forEach((data) =>{
+                textarea.value += data + "\n"
+            })
             count ++
         }else{
             new_data = `${count}, Couldn't deposit entered balance. (Insufficient cash balance)`
-            textarea.value += (textarea.value ? "\n" : "") + new_data
+            textarea.value = ""
+            arr.unshift(new_data)
+            arr.forEach((data) =>{
+                textarea.value += data + "\n"
+            })
             count ++
         }
     }else{
@@ -41,11 +55,19 @@ function handleOperate(){
             int_cash_balance += amount_convert
             int_account_balance -= amount_convert
             new_data = `${count}, Current balance: ${(int_account_balance).toString()}, Current cash balance: ${(int_cash_balance).toString()}`
-            textarea.value += (textarea.value ? "\n" : "") + new_data
+            textarea.value = ""
+            arr.unshift(new_data)
+            arr.forEach((data) =>{
+                textarea.value += data + "\n"
+            })
             count ++
         }else{
             new_data = `${count}, Couldn't withdraw entered balance. (Insufficient account balance)`
-            textarea.value += (textarea.value ? "\n" : "") + new_data
+            textarea.value = ""
+            arr.unshift(new_data)
+            arr.forEach((data) =>{
+                textarea.value += data + "\n"
+            })
             count ++
         }
     }
